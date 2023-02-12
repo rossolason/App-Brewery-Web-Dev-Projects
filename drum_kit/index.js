82e9a3e4170807dd122d97960f27e79d4e881e37
Ross Olason <rossolason@gmail.com>
2023-02-11 21:36:33 -0800

    Drum_Kit

---
+++ drum_kit/index.js
@@ -0,0 +1,56 @@
+var numButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var key = this.innerHTML;
+    makeSound(key);
+    animateKey(key);
+  });
+}
+
+document.addEventListener("keypress", function (event) {
+  makeSound(event.key);
+  animateKey(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "l":
+      var kickBass = new Audio("sounds/kick-bass.mp3");
+      kickBass.play();
+      break;
+    default:
+      console.log("Sound didn't play. key: " + key);
+  }
+}
+
+function animateKey(key) {
+  document.querySelector("." + key).classList.add("pressed");
+  setTimeout(function () {
+    document.querySelector("." + key).classList.remove("pressed");
+  }, 100);
+}
