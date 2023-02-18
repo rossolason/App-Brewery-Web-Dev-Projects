0771ea73fbe9d612af0b82c903a6a72e9d189ad3
Ross Olason <rossolason@gmail.com>
2023-02-17 21:46:40 -0800

    Todo_List_Start

---
+++ to_do_list/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+var items = ["Buy food", "Cook food", "Eat food"];
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", function (req, res) {
+  var today = new Date();
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  var day = today.toLocaleDateString("en-US", options);
+  res.render("List", {
+    kindOfDay: day,
+    newListItems: items,
+  });
+});
+
+app.post("/", function (req, res) {
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000.");
+});
